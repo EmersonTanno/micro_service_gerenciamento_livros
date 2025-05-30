90108ffa699c15bd53a218d844f4a67090f3a91b
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-30 14:06:41 -0300

    update: app.module - adding mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BooksModule],
+  imports: [ MongooseModule.forRoot('mongodb://localhost/micro-service-books'), BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
